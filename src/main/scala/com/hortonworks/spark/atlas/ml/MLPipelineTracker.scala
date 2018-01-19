0362dc07986485dded79fb40ea273514170c2f37
Mingjie Tang <mtang@hortonworks.com>
2018-01-19 12:59:03 -0800

    Spark ML-Atlas with ML piepline Listener

--- src/main/scala/com/hortonworks/spark/atlas/ml/MLPipelineTracker.scala
+++ src/main/scala/com/hortonworks/spark/atlas/ml/MLPipelineTracker.scala
@@ -17,11 +17,27 @@
 
 package com.hortonworks.spark.atlas.ml
 
-import com.hortonworks.spark.atlas.{AtlasClient, AtlasClientConf}
+import java.util.concurrent.{LinkedBlockingQueue, TimeUnit}
+
+import com.google.common.annotations.VisibleForTesting
+import com.hortonworks.spark.atlas.sql.AbstractService
+import com.hortonworks.spark.atlas.types.{AtlasEntityUtils, SparkAtlasModel, internal}
+import com.hortonworks.spark.atlas.utils.Logging
+import com.hortonworks.spark.atlas.{AtlasClient, AtlasClientConf, RestAtlasClient}
+import com.hortonworks.spark.atlas.utils.CatalogUtils
+
+import org.apache.atlas.model.instance.AtlasEntity
+import org.apache.spark.ml._
+import org.apache.spark.scheduler.{SparkListener, SparkListenerEvent}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+import scala.collection.mutable
+import scala.util.control.NonFatal
 
 class MLPipelineTracker(
     val atlasClient: AtlasClient,
-    val conf: AtlasClientConf) {
+    val conf: AtlasClientConf)
+  extends SparkListener with AbstractService with AtlasEntityUtils with Logging {
 
   def this(atlasClientConf: AtlasClientConf) = {
     this(AtlasClient.atlasClient(atlasClientConf), atlasClientConf)
@@ -31,5 +47,196 @@ class MLPipelineTracker(
     this(new AtlasClientConf)
   }
 
+  private val capacity = conf.get(AtlasClientConf.BLOCKING_QUEUE_CAPACITY).toInt
+
+  // A blocking queue for Spark Listener ExternalCatalog related events.
+  @VisibleForTesting
+  private[atlas] val eventQueue = new LinkedBlockingQueue[SparkListenerEvent](capacity)
+
+  private val timeout = conf.get(AtlasClientConf.BLOCKING_QUEUE_PUT_TIMEOUT).toInt
+
+  @VisibleForTesting
+  @volatile private[atlas] var shouldContinue: Boolean = true
+
+  private val cachedObjects = new mutable.HashMap[String, Object]
+
+  private val uri = "hdfs://"
+
+  private var tableEntity1:Seq[AtlasEntity] = null
+
+  startThread()
+
+  override def onOtherEvent(event: SparkListenerEvent): Unit = {
+    if (!shouldContinue) {
+      // No op if our tracker is failed to initialize itself
+      return
+    }
+
+    // We only care about ML related events.
+    event match {
+      case e: MLListenEvent =>
+        if (!eventQueue.offer(e, timeout, TimeUnit.MILLISECONDS)) {
+          logError(s"Fail to put event $e into queue within time limit $timeout, will throw it")
+        }
+
+      case _ =>
+      // Ignore other events
+    }
+  }
+
+  // Return table related entities as a Sequence.
+  // The first one is table entity, followed by
+  // db entity, storage entity and schema entities.
+  def getTableEntities(tableName: String): Seq[AtlasEntity] = {
+    val dbDefinition = CatalogUtils.createDB("db1", "hdfs:///test/db/db1")
+    val sd = CatalogUtils.createStorageFormat()
+    val schema = new StructType()
+      .add("user", StringType, false)
+      .add("age", IntegerType, true)
+    val tableDefinition = CatalogUtils.createTable("db1", s"$tableName", schema, sd)
+    val tableEntities = internal.sparkTableToEntities(tableDefinition, Some(dbDefinition))
+
+    tableEntities
+  }
+
   // Skeleton to track ML pipeline
+  @VisibleForTesting
+  protected[atlas] override def eventProcess(): Unit = {
+    // initialize Atlas client before further processing event.
+    if (!initializeSparkModel()) {
+      logError("Fail to initialize Atlas Client, will discard all the received events and stop " +
+        "working")
+
+      shouldContinue = false
+      eventQueue.clear()
+      return
+    }
+
+    var stopped = false
+    while (!stopped) {
+      try {
+        Option(eventQueue.poll(3000, TimeUnit.MILLISECONDS)).foreach {
+          case CreatePipelineEvent(pipeline, dataset) =>
+            cachedObjects.put(pipeline.uid, pipeline)
+            cachedObjects.put(pipeline.uid + "_" + "traindata", dataset)
+
+          case CreateModelEvent(pipelineModel) =>
+            cachedObjects.put(pipelineModel.uid + "_" + "model", pipelineModel)
+
+          case SavePipelineEvent(uid, path) =>
+            val pipelineDirEntity = internal.mlDirectoryToEntity(uri, path)
+            val pipeline = cachedObjects.get(uid).get.asInstanceOf[Pipeline]
+
+            val pipelineEntity = internal.mlPipelineToEntity(pipeline, pipelineDirEntity)
+            atlasClient.createEntities(Seq(pipelineEntity,pipelineDirEntity))
+
+            cachedObjects.put(uid + "_" + "pipelineDirEntity", pipelineDirEntity)
+            cachedObjects.put(uid + "_" + "pipelineEntity", pipelineEntity)
+
+            logInfo(s"Created pipeline Entity " + pipelineEntity.getGuid)
+
+          case SaveModelEvent(uid, path) =>
+
+            if (!cachedObjects.contains(uid + "_" + "pipelineDirEntity")) {
+
+                logInfo(s"Model Entity is already created")
+            } else {
+
+              val modelDirEntity = internal.mlDirectoryToEntity(uri, path)
+              cachedObjects.put(uid + "_" + "modelDirEntity", modelDirEntity)
+
+              val pipelineDirEntity = cachedObjects.get(uid + "_" + "pipelineDirEntity").get.asInstanceOf[AtlasEntity]
+              val pipelineEntity = cachedObjects.get(uid + "_" + "pipelineEntity").get.asInstanceOf[AtlasEntity]
+              val pipeline = cachedObjects.get(uid).get.asInstanceOf[Pipeline]
+
+              atlasClient.createEntities(Seq(pipelineDirEntity, modelDirEntity))
+              val model = cachedObjects.get(uid + "_" + "model").get.asInstanceOf[PipelineModel]
+
+              val modelEntity = internal.mlModelToEntity(model, modelDirEntity)
+              cachedObjects.put(uid + "_" + "modelEntity", modelEntity)
+
+              atlasClient.createEntities(Seq(modelEntity,modelDirEntity))
+
+              //to do list: get the dataframe entity from here
+              val tableEntities1 = getTableEntities("chris1")
+              tableEntity1 = tableEntities1
+
+              val fitEntity = internal.mlFitProcessToEntity(
+                pipeline,
+                pipelineEntity,
+                List(pipelineEntity, tableEntities1.head),
+                List(modelEntity))
+
+              atlasClient.createEntities(Seq(pipelineDirEntity, modelDirEntity,
+                pipelineEntity, modelEntity, fitEntity) ++ tableEntities1)
+
+              cachedObjects.remove(uid + "_" + "pipelineDirEntity")
+              cachedObjects.remove(uid + "_" + "pipelineEntity")
+              cachedObjects.remove(uid + "_" + "model")
+              cachedObjects.remove(uid)
+
+              logInfo(s"Created pipeline fitEntity " + fitEntity.getGuid)
+            }
+
+          case TransformEvent(model, dataset) =>
+
+            val uid = model.uid
+
+            if (cachedObjects.contains( uid + "_" + "modelEntity")) {
+
+              //to do list: get the dataframe entity from here
+              val tableEntities3 = getTableEntities("chris3")
+              val tableEntities2 = getTableEntities("chris2")
+
+              val modelEntity = cachedObjects.get(uid + "_" + "modelEntity").get.asInstanceOf[AtlasEntity]
+              val modelDirEntity = cachedObjects.get(uid + "_" + "modelDirEntity").get.asInstanceOf[AtlasEntity]
+
+              val transformEntity = internal.mlTransformProcessToEntity(
+                model,
+                modelEntity,
+                List(modelEntity, tableEntities3.head),
+                List(tableEntities2.head))
+
+              atlasClient.createEntities(Seq(modelDirEntity, modelEntity, transformEntity)
+                ++ tableEntities3 ++ tableEntities2)
+
+              cachedObjects.remove(uid + "_" + "modelEntity")
+              cachedObjects.remove(uid + "_" + "modelDirEntity")
+
+              logInfo(s"Created transFormEntity " + transformEntity.getGuid)
+              } else {
+
+                logInfo(s"Transform Entity is already created")
+            }
+
+          case _ =>
+            logInfo(s"ML tracker for other event")
+
+        }
+      }
+    }
+  }
+
+  private def initializeSparkModel(): Boolean = {
+    try {
+      val checkModelInStart = conf.get(AtlasClientConf.CHECK_MODEL_IN_START).toBoolean
+      if (checkModelInStart) {
+        val restClient = if (!atlasClient.isInstanceOf[RestAtlasClient]) {
+          logWarn("Spark Atlas Model check and creation can only work with REST client, so " +
+            "creating a new REST client")
+          new RestAtlasClient(conf)
+        } else {
+          atlasClient
+        }
+
+        SparkAtlasModel.checkAndCreateTypes(restClient)
+      }
+
+      true
+    } catch {
+      case NonFatal(e) =>
+        logError(s"Fail to initialize Atlas client, stop this listener", e)
+        false
+    }
+  }
 }
---
+++ src/test/scala/com/hortonworks/spark/atlas/ml/MLPipeLineEventTrackerEntitySuite.scala
@@ -0,0 +1,158 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hortonworks.spark.atlas.ml
+
+import scala.concurrent.duration._
+import com.hortonworks.spark.atlas.types.{SparkAtlasModel, internal, metadata}
+import com.hortonworks.spark.atlas.{AtlasClientConf, BaseResourceIT, RestAtlasClient}
+import org.apache.spark.ml._
+import org.apache.spark.ml.feature.MinMaxScaler
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.SparkSession
+import org.scalatest.Matchers
+import org.scalatest.concurrent.Eventually._
+
+
+class MLPipeLineEventTrackerEntitySuite extends BaseResourceIT with Matchers{
+
+  private var sparkSession: SparkSession = _
+
+  private var tracker: MLPipelineTracker = _
+
+  protected  override  val atlasClientConf = new AtlasClientConf()
+    .set(AtlasClientConf.CHECK_MODEL_IN_START.key, "false")
+    .set(AtlasClientConf.ATLAS_REST_ENDPOINT.key, "http://172.27.61.76:21000")
+
+  private val atlasClient = new RestAtlasClient(atlasClientConf)
+  SparkAtlasModel.checkAndCreateTypes(atlasClient)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    sparkSession = SparkSession.builder()
+      .master("local")
+      .config("spark.sql.catalogImplementation", "in-memory")
+      .getOrCreate()
+    tracker = new MLPipelineTracker(atlasClient, atlasClientConf)
+  }
+
+  override def afterAll(): Unit = {
+    Thread.sleep(5000L)
+    sparkSession.stop()
+    SparkSession.clearActiveSession()
+    SparkSession.clearDefaultSession()
+    sparkSession = null
+  }
+
+
+  test("Spark ML Pipeline save event to Atlas Entity ") {
+
+    val uri = "hdfs://"
+    val pipelineDir = "tmp/pipeline1"
+    val df = sparkSession.createDataFrame(Seq(
+      (1, Vectors.dense(0.0, 1.0, 4.0), 1.0),
+      (2, Vectors.dense(1.0, 0.0, 4.0), 2.0),
+      (3, Vectors.dense(1.0, 0.0, 5.0), 3.0),
+      (4, Vectors.dense(0.0, 0.0, 5.0), 4.0)
+    )).toDF("id", "features", "label")
+
+    val scaler = new MinMaxScaler()
+      .setInputCol("features")
+      .setOutputCol("features_scaled")
+      .setMin(0.0)
+      .setMax(3.0)
+
+    val pipeline = new Pipeline().setStages(Array(scaler))
+
+    val model = pipeline.fit(df)
+
+    pipeline.write.overwrite().save(pipelineDir)
+
+    tracker.onOtherEvent(CreatePipelineEvent(pipeline, df))
+    tracker.onOtherEvent(SavePipelineEvent(pipeline.uid, pipelineDir))
+  }
+
+  test("Spark PipelineModel save event to Atlas Entity")
+  {
+
+    val uri = "hdfs://"
+    val pipelineDir = "tmp/pipeline2"
+    val modelDir = "tmp/model2"
+
+    val df = sparkSession.createDataFrame(Seq(
+      (1, Vectors.dense(0.0, 1.0, 4.0), 1.0),
+      (2, Vectors.dense(1.0, 0.0, 4.0), 2.0),
+      (3, Vectors.dense(1.0, 0.0, 5.0), 3.0),
+      (4, Vectors.dense(0.0, 0.0, 5.0), 4.0)
+    )).toDF("id", "features", "label")
+
+    val scaler = new MinMaxScaler()
+      .setInputCol("features")
+      .setOutputCol("features_scaled")
+      .setMin(0.0)
+      .setMax(3.0)
+
+    val pipeline = new Pipeline().setStages(Array(scaler))
+
+    val model = pipeline.fit(df)
+
+    pipeline.write.overwrite().save(pipelineDir)
+    model.write.overwrite().save(modelDir)
+
+    tracker.onOtherEvent(CreatePipelineEvent(pipeline, df))
+    tracker.onOtherEvent(SavePipelineEvent(pipeline.uid, pipelineDir))
+    tracker.onOtherEvent(CreateModelEvent(model))
+    tracker.onOtherEvent(SaveModelEvent(model.uid, modelDir))
+  }
+
+
+  test("Spark PipelineModel transform event  to Atlas entities") {
+
+    val uri = "hdfs://"
+    val pipelineDir = "tmp/pipeline3"
+    val modelDir = "tmp/model3"
+
+    val df = sparkSession.createDataFrame(Seq(
+      (1, Vectors.dense(0.0, 1.0, 4.0), 1.0),
+      (2, Vectors.dense(1.0, 0.0, 4.0), 2.0),
+      (3, Vectors.dense(1.0, 0.0, 5.0), 3.0),
+      (4, Vectors.dense(0.0, 0.0, 5.0), 4.0)
+    )).toDF("id", "features", "label")
+
+    val scaler = new MinMaxScaler()
+      .setInputCol("features")
+      .setOutputCol("features_scaled")
+      .setMin(0.0)
+      .setMax(3.0)
+
+    val pipeline = new Pipeline().setStages(Array(scaler))
+    val model = pipeline.fit(df)
+
+    pipeline.write.overwrite().save(pipelineDir)
+    model.write.overwrite().save(modelDir)
+
+    val df2 = model.transform(df)
+    df2.collect()
+
+    tracker.onOtherEvent(CreatePipelineEvent(pipeline, df))
+    tracker.onOtherEvent(CreateModelEvent(model))
+    tracker.onOtherEvent(SavePipelineEvent(pipeline.uid, pipelineDir))
+    tracker.onOtherEvent(SaveModelEvent(model.uid, modelDir))
+    tracker.onOtherEvent(TransformEvent(model, df))
+  }
+
+}
