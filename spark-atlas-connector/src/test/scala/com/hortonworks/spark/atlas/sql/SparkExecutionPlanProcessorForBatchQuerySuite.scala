63ecca68543facf4818fcd38aabc95593df6bb5a
Jungtaek Lim <kabhwan@gmail.com>
2019-01-08 23:28:07 +0900

    [SAC-167][SQL] Support tracking create external table (#172)
    ## What changes were proposed in this pull request?
    
    This patch enables tracking the query like "CREATE TABLE USING xxx LOCATION" - external table. The patch also adds relevant UT.
    
    ## How was this patch tested?
    
    Existing UT as well as added UT. Manually tested against Spark 2.4 & Atlas 1.1
    
    spark-shell
    ```
    scala> spark.range(10).write.json("/tmp/1/json-sac-167")
    scala> spark.sql("CREATE TABLE test_json_788987 USING json location '/tmp/1/json-sac-167'")
    ```
    
    ![screen shot 2019-01-08 at 3 23 27 pm](https://user-images.githubusercontent.com/1317309/50813555-2299cd00-135a-11e9-87ce-0d4919cb7a23.png)
    ![screen shot 2019-01-08 at 3 23 35 pm](https://user-images.githubusercontent.com/1317309/50813557-23326380-135a-11e9-80e9-4537510bbcf0.png)
    
    This closes #167

--- spark-atlas-connector/src/test/scala/com/hortonworks/spark/atlas/sql/SparkExecutionPlanProcessorForBatchQuerySuite.scala
+++ spark-atlas-connector/src/test/scala/com/hortonworks/spark/atlas/sql/SparkExecutionPlanProcessorForBatchQuerySuite.scala
@@ -17,7 +17,7 @@
 
 package com.hortonworks.spark.atlas.sql
 
-import java.io.{BufferedWriter, FileWriter}
+import java.io.{BufferedWriter, File, FileWriter}
 import java.nio.file.{Files, Path}
 import java.util.Locale
 
@@ -26,6 +26,7 @@ import com.hortonworks.spark.atlas.types.{external, metadata}
 import com.hortonworks.spark.atlas.utils.SparkUtils
 import com.hortonworks.spark.atlas.AtlasClientConf
 import org.apache.atlas.model.instance.AtlasEntity
+import org.apache.commons.io.{FileUtils, IOUtils}
 import org.apache.spark.sql.streaming.StreamTest
 
 class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
@@ -49,6 +50,10 @@ class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
     super.afterAll()
   }
 
+  override def beforeEach(): Unit = {
+    atlasClient.clearEntities()
+  }
+
   test("Read csv file and save as table") {
     val planProcessor = new DirectProcessSparkExecutionPlanProcessor(atlasClient, atlasClientConf)
 
@@ -66,24 +71,8 @@ class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
     val entities = atlasClient.createdEntities
 
     val tableEntity: AtlasEntity = getOnlyOneEntity(entities, metadata.TABLE_TYPE_STRING)
-
-    // only assert qualifiedName and skip assertion on database, and attributes on database
-    // they should be covered in other UT
-    val tableQualifiedName = getStringAttribute(tableEntity, "qualifiedName")
-    assert(tableQualifiedName.endsWith(outputTableName))
-
-    // remove table name + '.' prior to table name
-    val databaseQualifiedName = tableQualifiedName.substring(0,
-      tableQualifiedName.indexOf(outputTableName) - 1)
-
-    val databaseEntity = getOnlyOneEntity(entities, metadata.DB_TYPE_STRING)
-    assert(getStringAttribute(databaseEntity, "qualifiedName") === databaseQualifiedName)
-
-    // database entity in table entity should be same as outer database entity
-    val databaseEntityInTable = getAtlasEntityAttribute(tableEntity, "db")
-    assert(databaseEntity === databaseEntityInTable)
-
-    val databaseLocationFsEntity = getAtlasEntityAttribute(databaseEntity, "locationUri")
+    assertTableEntity(tableEntity, outputTableName)
+    assertSchemaEntities(tableEntity, entities)
 
     // we're expecting three file system entities:
     // one for input file, one for database warehouse, and one for output table (under
@@ -91,19 +80,29 @@ class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
     val fsEntities = listAtlasEntitiesAsType(entities, external.FS_PATH_TYPE_STRING)
     assert(fsEntities.size === 3)
 
-    // database warehouse
-    assert(fsEntities.contains(databaseLocationFsEntity))
+    val databaseEntity = getOnlyOneEntity(entities, metadata.DB_TYPE_STRING)
+    assertDatabaseEntity(databaseEntity, tableEntity, fsEntities, outputTableName)
+
+    val databaseLocationFsEntity = getAtlasEntityAttribute(databaseEntity, "locationUri")
 
     val inputFsEntities = fsEntities.filterNot(_ == databaseLocationFsEntity)
     assert(inputFsEntities.size === 2)
 
     // input file
-    val inputFsEntity = inputFsEntities.head
+    // this code asserts on runtime that one of fs entity matches against source path
+    val sourcePath = tempFile.toAbsolutePath.toString
+    val inputFsEntity = inputFsEntities.find { p =>
+      getStringAttribute(p, "name").toLowerCase(Locale.ROOT) == sourcePath.toLowerCase(Locale.ROOT)
+    }.get
+    assertInputFsEntity(inputFsEntity, sourcePath)
 
-    assertPathsEquals(getStringAttribute(inputFsEntity, "name"), tempFile.toAbsolutePath.toString)
-    assertPathsEquals(getStringAttribute(inputFsEntity, "path"), tempFile.toAbsolutePath.toString)
-    assertPathsEquals(getStringAttribute(inputFsEntity, "qualifiedName"),
-      "file://" + tempFile.toAbsolutePath.toString)
+    // storage description
+    val storageEntity = getOnlyOneEntity(entities, metadata.STORAGEDESC_TYPE_STRING)
+    assertStorageDefinitionEntity(storageEntity, tableEntity)
+
+    val locationEntity = getAtlasEntityAttribute(storageEntity, "locationUri")
+    assert(getStringAttribute(locationEntity, "path").startsWith(
+      getStringAttribute(databaseLocationFsEntity, "path")))
 
     // check for 'spark_process'
     val processEntity = getOnlyOneEntity(entities, metadata.PROCESS_TYPE_STRING)
@@ -130,6 +129,55 @@ class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
     }
   }
 
+  test("Create external table against JSON files") {
+    val planProcessor = new DirectProcessSparkExecutionPlanProcessor(atlasClient, atlasClientConf)
+
+    val tempDirPath = writeJsonFilesToTempDirectory()
+    val tempDirPathStr = tempDirPath.toFile.getAbsolutePath
+
+    val rand = new scala.util.Random()
+    val outputTableName = "test_json_" + rand.nextInt(1000000000)
+    spark.sql(s"CREATE TABLE $outputTableName USING json location '$tempDirPathStr'")
+
+    val queryDetail = testHelperQueryListener.queryDetails.last
+    planProcessor.process(queryDetail)
+
+    val entities = atlasClient.createdEntities
+
+    val tableEntity: AtlasEntity = getOnlyOneEntity(entities, metadata.TABLE_TYPE_STRING)
+    assertTableEntity(tableEntity, outputTableName)
+    assertSchemaEntities(tableEntity, entities)
+
+    // we're expecting two file system entities:
+    // one for input file, one for database warehouse.
+    val fsEntities = listAtlasEntitiesAsType(entities, external.FS_PATH_TYPE_STRING)
+    assert(fsEntities.size === 2)
+
+    // database
+    val databaseEntity: AtlasEntity = getOnlyOneEntity(entities, metadata.DB_TYPE_STRING)
+    assertDatabaseEntity(databaseEntity, tableEntity, fsEntities, outputTableName)
+
+    val databaseLocationFsEntity = getAtlasEntityAttribute(databaseEntity, "locationUri")
+
+    // fs
+    val inputFsEntities = fsEntities.filterNot(_ == databaseLocationFsEntity)
+    assert(inputFsEntities.size === 1)
+
+    // input file
+    val inputFsEntity = inputFsEntities.find { p =>
+      getStringAttribute(p, "name").toLowerCase(Locale.ROOT) ==
+        tempDirPathStr.toLowerCase(Locale.ROOT)
+    }.get
+
+    assertInputFsEntity(inputFsEntity, tempDirPathStr)
+
+    // storage description
+    val storageEntity = getOnlyOneEntity(entities, metadata.STORAGEDESC_TYPE_STRING)
+    assertStorageDefinitionEntity(storageEntity, tableEntity)
+
+    assert(getAtlasEntityAttribute(storageEntity, "locationUri") === inputFsEntity)
+  }
+
   private def writeCSVtextToTempFile(csvContent: String) = {
     val tempFile = Files.createTempFile("spark-atlas-connector-csv-temp", ".csv")
 
@@ -145,6 +193,80 @@ class SparkExecutionPlanProcessorForBatchQuerySuite extends StreamTest {
     bw.write(csvContent)
     bw.close()
     tempFile
+  }
+
+  private def writeJsonFilesToTempDirectory(): Path = {
+    val tempParentDir = Files.createTempDirectory("spark-atlas-connector-json-temp")
+    val tempDir = new File(tempParentDir.toFile, "json")
+    val tempDirPath = tempDir.getAbsolutePath
+
+    org.apache.hadoop.util.ShutdownHookManager.get().addShutdownHook(
+      new Runnable {
+        override def run(): Unit = {
+          FileUtils.deleteQuietly(tempParentDir.toFile)
+        }
+      }, 10)
+
+    spark.range(10).write.json(tempDirPath)
+
+    tempDir.toPath
+  }
+
+  private def assertTableEntity(tableEntity: AtlasEntity, tableName: String): Unit = {
+    // only assert qualifiedName and skip assertion on database, and attributes on database
+    // they should be covered in other UT
+    val tableQualifiedName = getStringAttribute(tableEntity, "qualifiedName")
+    assert(tableQualifiedName.endsWith(tableName))
+  }
+
+  private def assertSchemaEntities(tableEntity: AtlasEntity, entities: Seq[AtlasEntity]): Unit = {
+    if (atlasClientConf.get(AtlasClientConf.ATLAS_SPARK_COLUMN_ENABLED).toBoolean) {
+      val columnEntities = listAtlasEntitiesAsType(entities, metadata.COLUMN_TYPE_STRING)
+      val columnEntitiesInTableAttribute = getSeqAtlasEntityAttribute(tableEntity, "spark_schema")
+      assert(Set(columnEntities) === Set(columnEntitiesInTableAttribute))
+    } else {
+      assert(!tableEntity.getAttributes.containsKey("spark_schema"))
+      assert(listAtlasEntitiesAsType(entities, metadata.COLUMN_TYPE_STRING).isEmpty)
+    }
+  }
+
+  private def assertDatabaseEntity(
+      databaseEntity: AtlasEntity,
+      tableEntity: AtlasEntity,
+      fsEntities: Seq[AtlasEntity],
+      tableName: String)
+    : Unit = {
+    val tableQualifiedName = getStringAttribute(tableEntity, "qualifiedName")
+    // remove table name + '.' prior to table name
+    val databaseQualifiedName = tableQualifiedName.substring(0,
+      tableQualifiedName.indexOf(tableName) - 1)
+
+    assert(getStringAttribute(databaseEntity, "qualifiedName") === databaseQualifiedName)
+
+    // database entity in table entity should be same as outer database entity
+    val databaseEntityInTable = getAtlasEntityAttribute(tableEntity, "db")
+    assert(databaseEntity === databaseEntityInTable)
+
+    val databaseLocationFsEntity = getAtlasEntityAttribute(databaseEntity, "locationUri")
+
+    // database warehouse
+    assert(fsEntities.contains(databaseLocationFsEntity))
+  }
+
+  private def assertInputFsEntity(fsEntity: AtlasEntity, sourcePath: String): Unit = {
+    assertPathsEquals(getStringAttribute(fsEntity, "name"), sourcePath)
+    assertPathsEquals(getStringAttribute(fsEntity, "path"), sourcePath)
+    assertPathsEquals(getStringAttribute(fsEntity, "qualifiedName"),
+      "file://" + sourcePath)
+  }
+
+  private def assertStorageDefinitionEntity(sdEntity: AtlasEntity, tableEntity: AtlasEntity): Unit = {
+    val tableQualifiedName = getStringAttribute(tableEntity, "qualifiedName")
+    val storageQualifiedName = tableQualifiedName + ".storageFormat"
+    assert(getStringAttribute(sdEntity, "qualifiedName") === storageQualifiedName)
+
+    val storageEntityInTableAttribute = getAtlasEntityAttribute(tableEntity, "sd")
+    assert(sdEntity === storageEntityInTableAttribute)
   }
 
   private def assertPathsEquals(path1: String, path2: String): Unit = {
