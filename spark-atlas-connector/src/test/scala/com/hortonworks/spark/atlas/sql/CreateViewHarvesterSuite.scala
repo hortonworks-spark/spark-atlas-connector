db0f282a31df8473ab5abbfa396744d9064320de
Bongani Shongwe <Bongani@users.noreply.github.com>
2019-03-27 07:22:15 +0100

    [SAC-197] Fix error when temporary views are created (#210)
    ## What changes were proposed in this pull request?
    
    When creating a temporary view, an exception would be thrown. This is because temporary items can not be obtained from the spark external catalogue. Thus, temporary must be ignored. In the code, it now only tracks PersistedViews; ignoring TemporaryViews and GlobalTemporaryViews. 
    
    ## How was this patch tested?
    
    Integration test
    
    Manual testing as done below:
    
    ` val df = spark.read.format("csv").option("header", "true").load("employee5.csv")
    df.createOrReplaceTempView("tempView")
    spark.sql("Select * from tempView").write.mode("overwrite").saveAsTable("employee")
    `
    Which produced the following lineage:
    ![Screenshot from 2019-03-23 23-01-32](https://user-images.githubusercontent.com/1842813/54872361-1c48b800-4dc3-11e9-9849-b649f8c2e8eb.png)
    
    Closes #210

--- spark-atlas-connector/src/test/scala/com/hortonworks/spark/atlas/sql/CreateViewHarvesterSuite.scala
+++ spark-atlas-connector/src/test/scala/com/hortonworks/spark/atlas/sql/CreateViewHarvesterSuite.scala
@@ -19,27 +19,42 @@ package com.hortonworks.spark.atlas.sql
 
 import java.util
 
+import com.hortonworks.spark.atlas.sql.testhelper.AtlasEntityReadHelper._
+
 import scala.collection.JavaConverters._
 import scala.util.Random
 import org.apache.atlas.AtlasClient
 import org.apache.atlas.model.instance.AtlasEntity
 import org.apache.spark.sql.execution.command.{CreateViewCommand, ExecutedCommandExec}
 import org.scalatest.{FunSuite, Matchers}
-
-import com.hortonworks.spark.atlas.types.external
-import com.hortonworks.spark.atlas.WithHiveSupport
+import com.hortonworks.spark.atlas.types.{external, metadata}
+import com.hortonworks.spark.atlas.{AtlasClientConf, WithHiveSupport}
+import com.hortonworks.spark.atlas.sql.testhelper.{AtlasQueryExecutionListener, CreateEntitiesTrackingAtlasClient, DirectProcessSparkExecutionPlanProcessor}
 
 
 class CreateViewHarvesterSuite extends FunSuite with Matchers with WithHiveSupport {
   private val sourceTblName = "source_" + Random.nextInt(100000)
   private val destinationViewName = "destination_" + Random.nextInt(100000)
   private val destinationViewName2 = "destination_" + Random.nextInt(100000)
+  private val destinationViewName3 = "destination_" + Random.nextInt(100000)
+  private val destinationTableName = "destination_" + Random.nextInt(100000)
+
+  private val testHelperQueryListener = new AtlasQueryExecutionListener()
+
+  var atlasClient: CreateEntitiesTrackingAtlasClient = _
+  val atlasClientConf: AtlasClientConf = new AtlasClientConf()
+    .set(AtlasClientConf.CHECK_MODEL_IN_START.key, "false")
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
 
     sparkSession.sql(s"CREATE TABLE $sourceTblName (name string)")
     sparkSession.sql(s"INSERT INTO TABLE $sourceTblName VALUES ('lucy'), ('tom')")
+
+    // setup Atlas client
+    testHelperQueryListener.clear()
+    atlasClient = new CreateEntitiesTrackingAtlasClient()
+    sparkSession.listenerManager.register(testHelperQueryListener)
   }
 
   test("CREATE VIEW FROM TABLE") {
@@ -96,5 +111,55 @@ class CreateViewHarvesterSuite extends FunSuite with Matchers with WithHiveSuppo
     outputView.getAttribute("name") should be (destinationViewName2)
     outputView.getAttribute(AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME).toString should endWith (
       s"default.$destinationViewName2")
+  }
+
+  test("CREATE TEMPORARY VIEW FROM TABLE, SAVE TEMP VIEW TO TABLE") {
+    val planProcessor = new DirectProcessSparkExecutionPlanProcessor(atlasClient, atlasClientConf)
+    sparkSession.sql(s"SELECT * FROM $sourceTblName").createOrReplaceTempView(destinationViewName3)
+
+    var queryDetail = testHelperQueryListener.queryDetails.last
+    planProcessor.process(queryDetail)
+    var entities = atlasClient.createdEntities
+
+    // no entities should have been received from a creating a temporary view
+    assert(entities.isEmpty)
+
+    // we don't want to check above queries, so reset the entities in listener
+    testHelperQueryListener.clear()
+
+    sparkSession.sql(s"SELECT * FROM $destinationViewName3").write.saveAsTable(destinationTableName)
+
+    queryDetail = testHelperQueryListener.queryDetails.last
+    planProcessor.process(queryDetail)
+    entities = atlasClient.createdEntities
+
+    // we're expecting two table entities:
+    // one from the source table and another from the sink table, the temporary view is ignored
+    assert(!entities.isEmpty)
+    val tableEntities = listAtlasEntitiesAsType(entities, metadata.TABLE_TYPE_STRING)
+    assert(tableEntities.size === 2)
+
+    val inputEntity = getOneEntityOnAttribute(tableEntities, "name", sourceTblName)
+    val outputEntity = getOneEntityOnAttribute(tableEntities, "name", destinationTableName)
+    assertTableEntity(inputEntity, sourceTblName)
+    assertTableEntity(outputEntity, destinationTableName)
+
+    // check for 'spark_process'
+    val processEntity = getOnlyOneEntity(entities, metadata.PROCESS_TYPE_STRING)
+    val inputs = getSeqAtlasEntityAttribute(processEntity, "inputs")
+    val outputs = getSeqAtlasEntityAttribute(processEntity, "outputs")
+    assert(inputs.size === 1)
+    assert(outputs.size === 1)
+
+    // input/output in 'spark_process' should be same as outer entities
+    val input = getOnlyOneEntity(inputs, metadata.TABLE_TYPE_STRING)
+    val output = getOnlyOneEntity(outputs, metadata.TABLE_TYPE_STRING)
+    assert(input === inputEntity)
+    assert(output === outputEntity)
+  }
+
+  private def assertTableEntity(entity: AtlasEntity, tableName: String): Unit = {
+    val tableQualifiedName = getStringAttribute(entity, "qualifiedName")
+    assert(tableQualifiedName.contains(s"$tableName"))
   }
 }
