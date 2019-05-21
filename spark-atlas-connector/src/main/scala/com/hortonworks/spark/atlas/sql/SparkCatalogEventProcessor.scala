a8af2c669e1403ff8e9fd4dfc552118e7f2ec633
Jungtaek Lim (HeartSaVioR) <kabhwan@gmail.com>
2019-05-21 18:02:45 +0900

    Reduce diff lines

--- spark-atlas-connector/src/main/scala/com/hortonworks/spark/atlas/sql/SparkCatalogEventProcessor.scala
+++ spark-atlas-connector/src/main/scala/com/hortonworks/spark/atlas/sql/SparkCatalogEventProcessor.scala
@@ -37,105 +37,106 @@ class SparkCatalogEventProcessor(
       // SAC will not handle any DDL events when remote HMS is used:
       // Hive hook will take care of all DDL events in Hive Metastore Service.
       // No-op here.
-    } else {
-      e match {
-        case CreateDatabasePreEvent(_) => // No-op
+      return
+    }
 
-        case CreateDatabaseEvent(db) =>
-          val dbDefinition = SparkUtils.getExternalCatalog().getDatabase(db)
-          val entity = sparkDbToEntity(dbDefinition)
-          atlasClient.createEntitiesWithDependencies(entity)
-          logDebug(s"Created db entity $db")
+    e match {
+      case CreateDatabasePreEvent(_) => // No-op
 
-        case DropDatabasePreEvent(db) =>
-          try {
-            cachedObject.put(sparkDbUniqueAttribute(db),
-              SparkUtils.getExternalCatalog().getDatabase(db))
-          } catch {
-            case _: NoSuchDatabaseException =>
-              logDebug(s"Spark already deleted the database: $db")
-          }
+      case CreateDatabaseEvent(db) =>
+        val dbDefinition = SparkUtils.getExternalCatalog().getDatabase(db)
+        val entity = sparkDbToEntity(dbDefinition)
+        atlasClient.createEntitiesWithDependencies(entity)
+        logDebug(s"Created db entity $db")
 
-        case DropDatabaseEvent(db) =>
-          atlasClient.deleteEntityWithUniqueAttr(sparkDbType, sparkDbUniqueAttribute(db))
+      case DropDatabasePreEvent(db) =>
+        try {
+          cachedObject.put(sparkDbUniqueAttribute(db),
+            SparkUtils.getExternalCatalog().getDatabase(db))
+        } catch {
+          case _: NoSuchDatabaseException =>
+            logDebug(s"Spark already deleted the database: $db")
+        }
 
-          cachedObject.remove(sparkDbUniqueAttribute(db)).foreach { o =>
-            val dbDef = o.asInstanceOf[CatalogDatabase]
-            val path = dbDef.locationUri.toString
-            val pathEntity = external.pathToEntity(path)
+      case DropDatabaseEvent(db) =>
+        atlasClient.deleteEntityWithUniqueAttr(sparkDbType, sparkDbUniqueAttribute(db))
 
-            atlasClient.deleteEntityWithUniqueAttr(pathEntity.entity.getTypeName,
-              AtlasEntityReadHelper.getQualifiedName(pathEntity.entity))
-          }
+        cachedObject.remove(sparkDbUniqueAttribute(db)).foreach { o =>
+          val dbDef = o.asInstanceOf[CatalogDatabase]
+          val path = dbDef.locationUri.toString
+          val pathEntity = external.pathToEntity(path)
 
-          logDebug(s"Deleted db entity $db")
+          atlasClient.deleteEntityWithUniqueAttr(pathEntity.entity.getTypeName,
+            AtlasEntityReadHelper.getQualifiedName(pathEntity.entity))
+        }
 
-        case CreateTablePreEvent(_, _) => // No-op
+        logDebug(s"Deleted db entity $db")
 
-        // TODO. We should also not create/alter view table in Atlas
-        case CreateTableEvent(db, table) =>
-          val tableDefinition = SparkUtils.getExternalCatalog().getTable(db, table)
-          val tableEntity = sparkTableToEntity(tableDefinition)
-          atlasClient.createEntitiesWithDependencies(tableEntity)
-          logDebug(s"Created table entity $table without columns")
+      case CreateTablePreEvent(_, _) => // No-op
 
-        case DropTablePreEvent(_, _) => // No-op
+      // TODO. We should also not create/alter view table in Atlas
+      case CreateTableEvent(db, table) =>
+        val tableDefinition = SparkUtils.getExternalCatalog().getTable(db, table)
+        val tableEntity = sparkTableToEntity(tableDefinition)
+        atlasClient.createEntitiesWithDependencies(tableEntity)
+        logDebug(s"Created table entity $table without columns")
 
-        case DropTableEvent(db, table) =>
-          logDebug(s"Can't handle drop table event since we don't have context information for " +
-            s"table $table in db $db. Can't delete table entity and corresponding entities.")
+      case DropTablePreEvent(_, _) => // No-op
 
-        case RenameTableEvent(db, name, newName) =>
-          // Update storageFormat's unique attribute
-          val sdEntity = new AtlasEntity(sparkStorageFormatType)
-          sdEntity.setAttribute(org.apache.atlas.AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME,
-            sparkStorageFormatUniqueAttribute(db, newName))
-          atlasClient.updateEntityWithUniqueAttr(
-            sparkStorageFormatType,
-            sparkStorageFormatUniqueAttribute(db, name),
-            sdEntity)
+      case DropTableEvent(db, table) =>
+        logDebug(s"Can't handle drop table event since we don't have context information for " +
+          s"table $table in db $db. Can't delete table entity and corresponding entities.")
 
-          // Update Table name and Table's unique attribute
-          val tableEntity = new AtlasEntity(sparkTableType)
-          tableEntity.setAttribute(org.apache.atlas.AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME,
-            sparkTableUniqueAttribute(db, newName))
-          tableEntity.setAttribute("name", newName)
-          atlasClient.updateEntityWithUniqueAttr(
-            sparkTableType,
-            sparkTableUniqueAttribute(db, name),
-            tableEntity)
+      case RenameTableEvent(db, name, newName) =>
+        // Update storageFormat's unique attribute
+        val sdEntity = new AtlasEntity(sparkStorageFormatType)
+        sdEntity.setAttribute(org.apache.atlas.AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME,
+          sparkStorageFormatUniqueAttribute(db, newName))
+        atlasClient.updateEntityWithUniqueAttr(
+          sparkStorageFormatType,
+          sparkStorageFormatUniqueAttribute(db, name),
+          sdEntity)
 
-          logDebug(s"Rename table entity $name to $newName")
+        // Update Table name and Table's unique attribute
+        val tableEntity = new AtlasEntity(sparkTableType)
+        tableEntity.setAttribute(org.apache.atlas.AtlasClient.REFERENCEABLE_ATTRIBUTE_NAME,
+          sparkTableUniqueAttribute(db, newName))
+        tableEntity.setAttribute("name", newName)
+        atlasClient.updateEntityWithUniqueAttr(
+          sparkTableType,
+          sparkTableUniqueAttribute(db, name),
+          tableEntity)
 
-        case AlterDatabaseEvent(db) =>
-          val dbDefinition = SparkUtils.getExternalCatalog().getDatabase(db)
-          val dbEntity = sparkDbToEntity(dbDefinition)
-          atlasClient.createEntitiesWithDependencies(dbEntity)
-          logDebug(s"Updated DB properties")
+        logDebug(s"Rename table entity $name to $newName")
 
-        case AlterTableEvent(db, table, kind) =>
-          val tableDefinition = SparkUtils.getExternalCatalog().getTable(db, table)
-          kind match {
-            case "table" =>
-              val tableEntity = sparkTableToEntityForAlterTable(tableDefinition)
-              atlasClient.createEntitiesWithDependencies(tableEntity)
-              logDebug(s"Updated table entity $table without columns")
+      case AlterDatabaseEvent(db) =>
+        val dbDefinition = SparkUtils.getExternalCatalog().getDatabase(db)
+        val dbEntity = sparkDbToEntity(dbDefinition)
+        atlasClient.createEntitiesWithDependencies(dbEntity)
+        logDebug(s"Updated DB properties")
 
-            case "dataSchema" =>
-              // We don't mind updating column
-              logDebug("Detected updating of table schema but ignored: " +
-                "column update will not be tracked here")
+      case AlterTableEvent(db, table, kind) =>
+        val tableDefinition = SparkUtils.getExternalCatalog().getTable(db, table)
+        kind match {
+          case "table" =>
+            val tableEntity = sparkTableToEntityForAlterTable(tableDefinition)
+            atlasClient.createEntitiesWithDependencies(tableEntity)
+            logDebug(s"Updated table entity $table without columns")
 
-            case "stats" =>
-              logDebug(s"Stats update will not be tracked here")
+          case "dataSchema" =>
+            // We don't mind updating column
+            logDebug("Detected updating of table schema but ignored: " +
+              "column update will not be tracked here")
 
-            case _ =>
-            // No op.
-          }
+          case "stats" =>
+            logDebug(s"Stats update will not be tracked here")
 
-        case f =>
-          logDebug(s"Drop unknown event $f")
-      }
+          case _ =>
+          // No op.
+        }
+
+      case f =>
+        logDebug(s"Drop unknown event $f")
     }
   }
 }
